2cf6ce79146bd5315a3a2907f57aaf715623c781
alec.schneider <alec.schneider@student.hpi.de>
2016-11-11 01:02:16 +0100

    Failing test for #2

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe AuthorsController, type: :controller do
         get :new
         expect(response).to render_template(:new)
     end
+
+    it "should display a field First name, Last name and Homepage" do
+        get :new
+        expect(response).to have_field("first name")
+        expect(response).to have_field("last name")
+        expect(response).to have_field("homepage")
+    end
 end
