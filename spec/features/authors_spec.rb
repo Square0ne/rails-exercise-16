49f0f634d332b2559bf0ee0d80307d96467357b0
alec.schneider <alec.schneider@student.hpi.de>
2016-11-12 20:24:12 +0100

    Failing test for #6

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -7,10 +7,23 @@ RSpec.feature "Authors", type: :feature do
 
     fill_in "First name", :with => "Alan"
     fill_in "Last name", :with => "Turing"
-    fill_in "Homepage", :with => "http://wikipedia.org/Alan_Turing"
+    fill_in "Homepage", :with => "http://wikipedia.de/Alan_Turing"
 
     click_button "create"
 
-    expect(Author.where(first_name:"Alan", last_name:"Turing", homepage:"http://wikipedia.org/Alan_Turing")).to exist
+    expect(Author.where(first_name:"Alan", last_name:"Turing", homepage:"http://wikipedia.de/Alan_Turing")).to exist
+  end
+
+  it "should display details" do
+    visit new_author_path
+
+    fill_in "First name", :with => "Alan"
+    fill_in "Last name", :with => "Turing"
+    fill_in "Homepage", :with => "http://wikipedia.de/Alan_Turing"
+
+    click_button "create"
+    expect(page).to have_text("Alan")
+    expect(page).to have_text("Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
   end
 end
