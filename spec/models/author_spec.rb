1abb829fb9e7cccaf5c1d91e0490427e20248b29
alec.schneider <alec.schneider@student.hpi.de>
2016-11-12 19:20:59 +0100

    Failing test for #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Author, type: :model do
     it "should create new Author" do
         Author.new(first_name:"Alan", last_name:"Turing", homepage:"http://wikipedia.org/Alan_Turing")
     end
+    it "should have a name" do
+        expect((Author.new(first_name:"Alan", last_name:"Turing", homepage:"http://wikipedia.org/Alan_Turing")).name).to eq "Alan Turing"
+    end
+
 end
