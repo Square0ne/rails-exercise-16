b74d854ff7e690d9a2231168d5fcadf2c92693de
alec.schneider <alec.schneider@student.hpi.de>
2016-11-13 18:45:56 +0100

    Failing test for #22

--- spec/controllers/papers_controller_spec.rb
+++ spec/controllers/papers_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PapersController, type: :controller do
-
+    it "render edit page" do
+       @example= create(:paper)
+       get :edit, id: @example.id
+       expect(response).to render_template("edit")
+    end
 end
