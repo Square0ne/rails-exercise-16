a98f06b7d35a6722ad8c713b27b59719dd83f688
alec.schneider <alec.schneider@student.hpi.de>
2016-11-13 18:27:25 +0100

    Failing test for #20

--- spec/features/papers_spec.rb
+++ spec/features/papers_spec.rb
@@ -4,4 +4,11 @@ RSpec.feature "Papers", type: :feature do
     it "should have a new page" do
         visit new_paper_path
     end
+    it "should have an index page" do
+      visit papers_path
+
+      expect(page).to have_text("Title")
+      expect(page).to have_text("Venue")
+      expect(page).to have_text("Year")
+    end
 end
