7f9b8bb98bdcdc4b75d2a8ae8dbc85e46516d397
alec.schneider <alec.schneider@student.hpi.de>
2016-11-13 20:47:38 +0100

    added empty array of papers for author Closes #25

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,12 @@
 class Author < ActiveRecord::Base
+    attr_accessor :papers
     validates :last_name, presence: true
     def name
         first_name + " " + last_name
     end
+    after_initialize :default_values
+    private
+       def default_values
+         self.papers ||= Array.new()
+       end
 end
