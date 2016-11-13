ffee87b9e7bd3d0cb043bef109c86d731d1e099b
alec.schneider <alec.schneider@student.hpi.de>
2016-11-13 14:00:33 +0100

    show error Closes #11

--- spec/views/authors/new.html.erb_spec.rb
+++ spec/views/authors/new.html.erb_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "authors/new", type: :view do
+    before :each do
+		assign(:author, Author.new)
+	end
+    
     it "should display a field First name, Last name and Homepage" do
         render :template => "authors/new.html.erb"
         rendered.should have_field("First name")
