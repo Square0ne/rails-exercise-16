ab15af8273400dfd35e5aa2a9568e027ab469395
alec.schneider <alec.schneider@student.hpi.de>
2016-11-13 14:23:44 +0100

    added edit page Closes #12

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,12 +8,25 @@ class AuthorsController < ApplicationController
     def new
         @author = Author.new
     end
+    def edit
+        @author = Author.find(params[:id])
+    end
     def create
         @author = Author.new(author_params)
         if @author.save
             redirect_to @author
         else
             render 'new'
+        end
+    end
+
+    def update
+        @author = Author.find(params[:id])
+
+        if @author.update(author_params)
+            redirect_to @author
+        else
+            render 'edit'
         end
     end
 
