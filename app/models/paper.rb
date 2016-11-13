32c173bb9e8a02408e92dd526db9d9f59a6663dd
alec.schneider <alec.schneider@student.hpi.de>
2016-11-13 19:17:02 +0100

    added empty array of authors for paper Closes #24

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,12 @@
 class Paper < ActiveRecord::Base
+    attr_accessor :authors
     validates :title, presence: true
     validates :venue, presence: true
     validates :year, presence: true, numericality: { only_integer: true }
+    after_initialize :default_values
+
+    private
+       def default_values
+         self.authors ||= Array.new()
+       end
 end
